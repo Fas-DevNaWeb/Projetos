b126db4b144c4f1a71fa3430cbbd6fc1336a6501
Fabiano <fasnawebdev@gmail.com>
2024-07-27 14:36:23 -0300

    Reorganizando

---
+++ Promese/script.js
@@ -0,0 +1,9 @@
+const btnEscolhaCao = document.querySelector('.escolha-cao');
+const btnEscolhaGato = document.querySelector('.escolha-gato');
+const btnSurpreendaMe = document.querySelector('.surpreenda-me');
+const btnImage = document.querySelector('.image');
+
+console.log(btnEscolhaCao);
+console.log(btnEscolhaGato);
+console.log(btnSurpreendaMe);
+console.log(btnImage);
