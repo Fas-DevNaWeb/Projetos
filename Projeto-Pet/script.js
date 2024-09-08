a6ecbe7773e1081778572d55e5f6f8bd0cc3eb78
Fabiano <fasnawebdev@gmail.com>
2024-09-08 10:00:38 -0300

    Api funcionando com sucesso

--- Projeto-Pet/script.js
+++ Projeto-Pet/script.js
@@ -1,1 +1,18 @@
-alert('teste!!!');
+const btn__dog = document.querySelector('.get-dog');
+const btn__cat = document.querySelector('.get-cat');
+const btn__surprise = document.querySelector('.get-surprise');
+const img__pet = document.querySelector(".img-pet");
+
+//console.log(btn__dog);
+const buscarApiDog = () => {
+  const promise = fetch("https://dog.ceo/api/breeds/image/random")
+    .then((resposta) => {
+      return resposta.json();
+    })
+    .then((data) => {
+      img__pet.src = data.message;
+    });
+};
+
+
+buscarApiDog();
