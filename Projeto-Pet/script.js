e21542a42d363d5fe573f14c50f13b5375a5d50f
Fabiano <fasnawebdev@gmail.com>
2024-09-08 10:31:56 -0300

    Api do cat funcionando corretamante

--- Projeto-Pet/script.js
+++ Projeto-Pet/script.js
@@ -1,18 +1,33 @@
-const btn__dog = document.querySelector('.get-dog');
-const btn__cat = document.querySelector('.get-cat');
-const btn__surprise = document.querySelector('.get-surprise');
+const btn__dog = document.querySelector(".get-dog");
+const btn__cat = document.querySelector(".get-cat");
+const btn__surprise = document.querySelector(".get-surprise");
 const img__pet = document.querySelector(".img-pet");
 
 //console.log(btn__dog);
 const buscarApiDog = () => {
-  const promise = fetch("https://dog.ceo/api/breeds/image/random")
-    .then((resposta) => {
-      return resposta.json();
+  const promiseDog = fetch("https://dog.ceo/api/breeds/image/random")
+    .then((respostaDog) => {
+      return respostaDog.json();
     })
-    .then((data) => {
-      img__pet.src = data.message;
+    .then((dataDog) => {
+      img__pet.src = dataDog.message;
     });
 };
 
+const buscarApiCat = () => {
+  const promiseCat = fetch("https://api.thecatapi.com/v1/images/search")
+    .then((respostaCat) => {
+      return respostaCat.json();
+    })
+    .then((dataCat) => {
+      img__pet.src = dataCat[0].url;
+    });
+};
 
-buscarApiDog();
+btn__dog.addEventListener("click", () => {
+  buscarApiDog();
+});
+
+btn__cat.addEventListener("click", () => {
+  buscarApiCat();
+});
