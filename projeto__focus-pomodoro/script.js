cd0d0611151cb9dfe868ff4bc221d1ab7a6fa298
Fabiano <fasnawebdev@gmail.com>
2024-10-19 22:02:34 -0300

    Projeto finalizado de acordo com a proposta

--- projeto__focus-pomodoro/script.js
+++ projeto__focus-pomodoro/script.js
@@ -7,11 +7,22 @@ const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button')
 const musicaFoco = document.querySelector('#alternar-musica')
 const musica = new Audio('./sons/luna-rise-part-one.mp3')
+const btnComecarPause = document.querySelector('#start-pause')
+const fraseDoBotao = document.querySelector('#start-pause span')
+const somDePlay = new Audio('./sons/play.wav')
+const somDePause = new Audio('./sons/pause.mp3')
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
+const imgIconPlay = document.querySelector('.app__card-primary-butto-icon')
+const tempoNaTela = document.querySelector('#timer')
 
-console.log(musicaFoco)
+
+let intervaloId = null
+let tempoDecorridoEmSegundos = 1500
+
 musica.loop = true //Musica vai ficar tocando continuamente.
 
 function alterarContexto(contexto) {
+    mostrarTempo()
     botoes.forEach(function (contexto) {
         contexto.classList.remove('active')
     })
@@ -30,18 +41,20 @@ function alterarContexto(contexto) {
     }
 }
 
-
 btnFoco.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500
     alterarContexto('foco')
     btnFoco.classList.add('active')
 })
 
 btnDescansoCurto.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300
     alterarContexto('descanso-curto')
     btnDescansoCurto.classList.add('active')
 })
 
 btnDescancoLongo.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900
     alterarContexto('descanso-longo')
     btnDescancoLongo.classList.add('active')
 })
@@ -53,3 +66,46 @@ musicaFoco.addEventListener('change', () => {
         musica.pause()
     }
 })
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        // audioTempoFinalizado.play()
+        alert('Tempo finalisado.')
+        zerar()
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    mostrarTempo()
+}
+
+btnComecarPause.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if (btnComecarPause.innerText == 'Começar') {
+        somDePlay.play()
+        imgIconPlay.setAttribute('src', 'imagens/pause.png')
+        fraseDoBotao.textContent = 'Pause'
+    } else if (btnComecarPause.innerText == 'Pause') {
+        somDePause.play()
+        imgIconPlay.setAttribute('src', 'imagens/play_arrow.png')
+        fraseDoBotao.textContent = 'Começar'
+    }
+
+    if (intervaloId) {
+        zerar()
+        return
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
+}
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-br', { minute: '2-digit', second: '2-digit' })
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+mostrarTempo()
--- sobreJavaScript.js
+++ sobreJavaScript.js
@@ -35,10 +35,12 @@ addEventListener(evento, callback)
     removeAttribute('nome do atributo') - É usado para remover um atributo de um elemento Html 
 
 
-SOBRE O MÉTODO innerHTML
+SOBRE O MÉTODO INNERHTML
     É utilizado para inserir textos ou adicionar elementos dentro de outra tag html. 
 
-SOBRE O MÉTODO ClassList
+    Já o método textContent só aceita texto não tags html 
+
+SOBRE O MÉTODO ClASSLIST
     classList é uma propriedade do JavaScript que representa uma lista de classes CSS.
     Ela fornece métodos que facilitam a adição, remoção e verificação de classes, 
     tornando a manipulação de classes CSS mais eficiente e menos suscetível a erros de programação.
@@ -46,7 +48,9 @@ SOBRE O MÉTODO ClassList
     São utilizados com ela os métodos - add(), remove(), toggle(), contains()
 
 
-MANIPULANDO Audio
+
+
+MANIPULANDO AUDIO
     Para criar um novo objeto ‘Audio’, podemos simplesmente usar a seguinte sintaxe:
     const audioElement = new Audio('caminho/do/arquivo-de-audio.mp3');
 
@@ -60,3 +64,49 @@ MANIPULANDO Audio
     audioElement.pause(); // Pausa a reprodução
     audioElement.currentTime = 10; // Move para 10 segundos no áudio
     audioElement.volume = 0.5; // Define o volume para metade (50%)
+
+    OBJETO DATE - VÁRIAS FORMAS DE CONSTRUÇÃO
+    
+    Construtor sem argumentos
+    const dataAtual = new Date();
+
+    Construtor com argumentos (ano, mês, dia, hora, minuto, segundo, milissegundo)
+    const dataEspecifica = new Date(2023, 7, 3, 12, 30, 0, 0);
+
+    Construtor com uma string que representa a data (formato padrão é "yyyy-mm-dd")
+    const dateString = "2023-08-03";
+    const formatoDeData = new Date(dateString);
+
+    Uma vez criado um objeto Date, você pode acessar informações específicas da data 
+    e hora, como o ano, mês, dia, hora, minuto e segundo usando os métodos de acesso 
+    do objeto.
+    Exemplo:
+    const currentDate = new Date();
+
+    const ano = currentDate.getFullYear();  // Acessa o ano
+    const mês = currentDate.getMonth(); // Acessa o mês - Janeiro é 0, Fevereiro é 1, ..., Dezembro é 11
+    const dia = currentDate.getDate(); // Acessa o dia
+    const horas = currentDate.getHours(); // Acessa as horas 
+    const minutos = currentDate.getMinutes(); // Acessa os minutos
+    const segundos = currentDate.getSeconds(); // Acessa os segundos
+    const milissegundos = currentDate.getMilliseconds();  // Acessa os milissegundos 
+
+    Você também pode modificar a data e hora usando os métodos de definição.
+
+    const data = new Date();
+
+    data.setFullYear(2024);  // Define o ano
+    data.setMonth(10); // Define o mês
+    data.setDate(25); // Define o dia
+    data.setHours(10);  // Define as horas
+    data.setMinutes(30); // Define os minutos
+    data.setSeconds(0); // Define os segundos
+
+    Além disso, o objeto ‘Date’ também fornece vários métodos para trabalhar com 
+    datas, como comparar datas, adicionar ou subtrair períodos de tempo, obter o 
+    dia da semana, entre outros.
+    Lembre-se de que, dependendo do fuso horário e configurações regionais do 
+    sistema em que o código está sendo executado, os resultados podem variar. 
+    Se você precisar de mais funcionalidades e suportes a diferentes formatos de 
+    zonas de tempo, uma boa alternativa é o uso de bibliotecas de manipulação de 
+    datas, como o Moment.js ou o date-fns.
