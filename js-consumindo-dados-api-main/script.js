9d547ad56c52428a42b43ab1038b9648a1c98573
Fabiano <fasnawebdev@gmail.com>
2024-10-21 22:24:58 -0300

    implementando formulario utilisando promeses

---
+++ js-consumindo-dados-api-main/script.js
@@ -0,0 +1,50 @@
+/**
+ * O corpo da resposta de uma requisição chega em formato de bytes. 
+ * Desta forma o .json() transforma o corpo e retorna um json formatado. 
+ * O formato JSON (JavaScript Object Notation) possui basicamente a mesma 
+ * sintaxe que a de um objeto JS.
+ */
+
+/**
+// Aqui você esta fazendo a requisição da promise
+const cepRecebido = fetch('http://viacep.com.br/ws/01001000/json/')
+
+    // Aqui você pega a resposta da promessa e converte para um formato JSON()
+    .then(response => response.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error('Esse cep não existe')
+        } else {
+            console.log(r)
+        }
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => {
+        console.log('Processamento concluido')
+    })
+*/
+
+// OUTRA MANEIRA DE FAZER CODIGO ASSINCRONO
+
+// async indica que essa é uma função assincrona
+async function buscarCep(cep) {
+    try { // Envolve todo código que esta correto
+        const consultarCep = await fetch(`http://viacep.com.br/ws/${cep}/json/`)
+        const constresposta = await consultarCep.json()
+        if(constresposta.erro){
+            throw Error('CEP não Existe :(')
+        }
+        console.log(constresposta)
+        return constresposta
+    } catch (erro) {//Envolve o código de erro
+        console.log(erro)
+    }
+}
+
+
+/*
+let ceps = ['01001000','01001001']
+let conjuntosCeps = ceps.map(valores => buscarCep(valores))
+Promise.all(conjuntosCeps).then(respostas => console.log(respostas))
+buscarCep()
+*/
--- sobreJavaScript.js
+++ sobreJavaScript.js
@@ -13,26 +13,26 @@ SOBRE O DEFER(adiar)
 
 SOBRE EVENTOS
 addEventListener(evento, callback)
-    elemento: É o elemento HTML ao qual queremos associar o evento.
+elemento: É o elemento HTML ao qual queremos associar o evento.
     evento: É uma string que representa o tipo de evento que desejamos capturar.
-    callback: É a função que será chamada quando o evento ocorrer.
+        callback: É a função que será chamada quando o evento ocorrer.
 
-    ex: Elemento html recuperado 
-        const btnTeste = document.querySelector('.teste')
-        btnTeste.addEventListener('click', () => {
-            console.log('Descreva seu codigo aqui dentro da função')
-        })
+            ex: Elemento html recuperado
+const btnTeste = document.querySelector('.teste')
+btnTeste.addEventListener('click', () => {
+    console.log('Descreva seu codigo aqui dentro da função')
+})
     Você pode utilizar outros tipos de eventos como:
-        'sibmit' - Para submeter formulários
-        'keydown'- Ocorre quando uma tecla é precionada.
-        'keyup'  - Ocorre quando uma tecla é solta.
+'sibmit' - Para submeter formulários
+'keydown' - Ocorre quando uma tecla é precionada.
+        'keyup' - Ocorre quando uma tecla é solta.
         'keypress' - Ocorre quando uma telca é precionada e ainda não foi solta.
 
  TRABALHANDO COM ATRIBUTOS
-    getAttribute('nome do atributo que queremos recuperar') - É usado para obter o valor de um atributo especifico de um elemento.       
-    setAttribute('atributo a ser setado','novo valor') - Modifica o valor de um atributo do elemento html
+getAttribute('nome do atributo que queremos recuperar') - É usado para obter o valor de um atributo especifico de um elemento.
+    setAttribute('atributo a ser setado', 'novo valor') - Modifica o valor de um atributo do elemento html
     hasAttribute('nome do atributo') - É usado para cerificar se um elemento possui o atributo especificado retorna verdadeiro ou false
-    removeAttribute('nome do atributo') - É usado para remover um atributo de um elemento Html 
+removeAttribute('nome do atributo') - É usado para remover um atributo de um elemento Html 
 
 
 SOBRE O MÉTODO INNERHTML
@@ -42,7 +42,7 @@ SOBRE O MÉTODO INNERHTML
 
 SOBRE O MÉTODO ClASSLIST
     classList é uma propriedade do JavaScript que representa uma lista de classes CSS.
-    Ela fornece métodos que facilitam a adição, remoção e verificação de classes, 
+    Ela fornece métodos que facilitam a adição, remoção e verificação de classes,
     tornando a manipulação de classes CSS mais eficiente e menos suscetível a erros de programação.
 
     São utilizados com ela os métodos - add(), remove(), toggle(), contains()
@@ -52,61 +52,78 @@ SOBRE O MÉTODO ClASSLIST
 
 MANIPULANDO AUDIO
     Para criar um novo objeto ‘Audio’, podemos simplesmente usar a seguinte sintaxe:
-    const audioElement = new Audio('caminho/do/arquivo-de-audio.mp3');
+const audioElement = new Audio('caminho/do/arquivo-de-audio.mp3');
 
     Controles básicos de áudio
-    play(): inicia a reprodução do áudio;
+play(): inicia a reprodução do áudio;
     pause(): pausa a reprodução do áudio;
     currentTime: propriedade que retorna ou define a posição atual de reprodução do áudio, em segundos;
-    volume: propriedade que retorna ou define o nível de volume do áudio, variando de 0 a 1.
+volume: propriedade que retorna ou define o nível de volume do áudio, variando de 0 a 1.
 
-    audioElement.play(); // Inicia a reprodução
-    audioElement.pause(); // Pausa a reprodução
-    audioElement.currentTime = 10; // Move para 10 segundos no áudio
-    audioElement.volume = 0.5; // Define o volume para metade (50%)
+audioElement.play(); // Inicia a reprodução
+audioElement.pause(); // Pausa a reprodução
+audioElement.currentTime = 10; // Move para 10 segundos no áudio
+audioElement.volume = 0.5; // Define o volume para metade (50%)
 
     OBJETO DATE - VÁRIAS FORMAS DE CONSTRUÇÃO
     
     Construtor sem argumentos
-    const dataAtual = new Date();
+const dataAtual = new Date();
 
-    Construtor com argumentos (ano, mês, dia, hora, minuto, segundo, milissegundo)
-    const dataEspecifica = new Date(2023, 7, 3, 12, 30, 0, 0);
+    Construtor com argumentos(ano, mês, dia, hora, minuto, segundo, milissegundo)
+const dataEspecifica = new Date(2023, 7, 3, 12, 30, 0, 0);
 
-    Construtor com uma string que representa a data (formato padrão é "yyyy-mm-dd")
-    const dateString = "2023-08-03";
-    const formatoDeData = new Date(dateString);
+    Construtor com uma string que representa a data(formato padrão é "yyyy-mm-dd")
+const dateString = "2023-08-03";
+const formatoDeData = new Date(dateString);
 
     Uma vez criado um objeto Date, você pode acessar informações específicas da data 
-    e hora, como o ano, mês, dia, hora, minuto e segundo usando os métodos de acesso 
-    do objeto.
+    e hora, como o ano, mês, dia, hora, minuto e segundo usando os métodos de acesso
+do objeto.
     Exemplo:
-    const currentDate = new Date();
+const currentDate = new Date();
 
-    const ano = currentDate.getFullYear();  // Acessa o ano
-    const mês = currentDate.getMonth(); // Acessa o mês - Janeiro é 0, Fevereiro é 1, ..., Dezembro é 11
-    const dia = currentDate.getDate(); // Acessa o dia
-    const horas = currentDate.getHours(); // Acessa as horas 
-    const minutos = currentDate.getMinutes(); // Acessa os minutos
-    const segundos = currentDate.getSeconds(); // Acessa os segundos
-    const milissegundos = currentDate.getMilliseconds();  // Acessa os milissegundos 
+const ano = currentDate.getFullYear();  // Acessa o ano
+const mês = currentDate.getMonth(); // Acessa o mês - Janeiro é 0, Fevereiro é 1, ..., Dezembro é 11
+const dia = currentDate.getDate(); // Acessa o dia
+const horas = currentDate.getHours(); // Acessa as horas 
+const minutos = currentDate.getMinutes(); // Acessa os minutos
+const segundos = currentDate.getSeconds(); // Acessa os segundos
+const milissegundos = currentDate.getMilliseconds();  // Acessa os milissegundos 
 
     Você também pode modificar a data e hora usando os métodos de definição.
 
-    const data = new Date();
+const data = new Date();
 
-    data.setFullYear(2024);  // Define o ano
-    data.setMonth(10); // Define o mês
-    data.setDate(25); // Define o dia
-    data.setHours(10);  // Define as horas
-    data.setMinutes(30); // Define os minutos
-    data.setSeconds(0); // Define os segundos
+data.setFullYear(2024);  // Define o ano
+data.setMonth(10); // Define o mês
+data.setDate(25); // Define o dia
+data.setHours(10);  // Define as horas
+data.setMinutes(30); // Define os minutos
+data.setSeconds(0); // Define os segundos
 
-    Além disso, o objeto ‘Date’ também fornece vários métodos para trabalhar com 
-    datas, como comparar datas, adicionar ou subtrair períodos de tempo, obter o 
+    Além disso, o objeto ‘Date’ também fornece vários métodos para trabalhar com
+datas, como comparar datas, adicionar ou subtrair períodos de tempo, obter o 
     dia da semana, entre outros.
-    Lembre-se de que, dependendo do fuso horário e configurações regionais do 
+    Lembre - se de que, dependendo do fuso horário e configurações regionais do 
     sistema em que o código está sendo executado, os resultados podem variar. 
     Se você precisar de mais funcionalidades e suportes a diferentes formatos de 
-    zonas de tempo, uma boa alternativa é o uso de bibliotecas de manipulação de 
-    datas, como o Moment.js ou o date-fns.
+    zonas de tempo, uma boa alternativa é o uso de bibliotecas de manipulação de
+datas, como o Moment.js ou o date - fns.
+
+    SOBRE PROMISE
+
+    Resumo de como obter os valores do objeto de uma promessa.
+
+// Aqui temos a requisição da promessa, com a função fetch()
+const teste = fetch('http://viacep.com.br/ws/01001000/json/')
+
+    // Aqui você pega o retorno da promessa e comverte em .json(),
+    //não se esqueça de fazer o retorno dessa resposta.
+    .then(response => {
+        return response.json()
+    })
+    // Aqui você consegue acessar o objeto data retornado
+    .then(data => {
+        console.log(data.estado)
+    })
