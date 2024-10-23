8473d836ec3766918e63b9ef093ebf013e788920
Fabiano <fasnawebdev@gmail.com>
2024-10-22 21:09:40 -0300

    Ajustando projeto pet

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -24,23 +24,6 @@ const cepRecebido = fetch('http://viacep.com.br/ws/01001000/json/')
     })
 */
 
-// OUTRA MANEIRA DE FAZER CODIGO ASSINCRONO
-
-// async indica que essa é uma função assincrona
-async function buscarCep(cep) {
-    try { // Envolve todo código que esta correto
-        const consultarCep = await fetch(`http://viacep.com.br/ws/${cep}/json/`)
-        const constresposta = await consultarCep.json()
-        if(constresposta.erro){
-            throw Error('CEP não Existe :(')
-        }
-        console.log(constresposta)
-        return constresposta
-    } catch (erro) {//Envolve o código de erro
-        console.log(erro)
-    }
-}
-
 
 /*
 let ceps = ['01001000','01001001']
@@ -48,3 +31,32 @@ let conjuntosCeps = ceps.map(valores => buscarCep(valores))
 Promise.all(conjuntosCeps).then(respostas => console.log(respostas))
 buscarCep()
 */
+
+
+// OUTRA MANEIRA DE FAZER CODIGO ASSINCRONO
+
+// async indica que essa é uma função assincrona
+async function buscarCep(cep) {
+    try { // Envolve todo código que esta correto
+        const consultarCep = await fetch(`http://viacep.com.br/ws/${cep}/json/`)
+        const constresposta = await consultarCep.json()
+        if (constresposta.erro) {
+            throw Error('CEP não Existe :(')
+        }
+        const cidade = document.getElementById9('cidade')
+        const logradouro = document.getElementById('endereco')
+        const estado = document.getElementById('estado')
+
+        cidade.value = constresposta.cidade
+        logradouro.value = constresposta.endereco
+        estado.value = constresposta.uf
+
+        console.log(constresposta)
+        return constresposta
+    } catch (erro) {//Envolve o código de erro
+        console.log(erro)
+    }
+}
+
+const cep = document.querySelector('#cep')
+cep.addEventListener('focusout', () => buscarCep(cep.value))
