239875ffd1d9601af7b320ccfc251811ef723ec4
Fabiano <fasnawebdev@gmail.com>
2024-10-27 17:43:00 -0300

    reorganizando projeto

---
+++ anotacoes/anotacoes_sobre_node.js
@@ -0,0 +1,13 @@
+
+  Node é uma ferramenta baseada em javaScript ,voltada para o lado do back end.Ou seja você 
+  não precisa do navegsdor para rodar scripts js.
+  
+  Comandos node:
+  node -v ou node -version -> Trás a versão do node.
+  o cmando node abre o terminal para digitar códigos javaScript, para sair digite crtl+d ou crtl+ cc
+  
+  Arquivo package.json -> É um arquivo manifesto, nesse arquivo esta escrito todos as informações principais 
+  do Projeto.Exemplo, o nome do projeto,o repositório do git onde ele esta guardade,a versão...
+
+  Npm e Yarn ->  São gerenciadores de pacotes, então quando voce precisa de certa dependencia ou pacote 
+  voce instala atraves do node ou do Yarn.
