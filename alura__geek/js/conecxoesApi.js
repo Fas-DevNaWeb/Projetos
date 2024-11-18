a8124a7b39aeabf630f911f14d25b05e039d14a1
Fabiano <fasnaweb2020@gmail.com>
2024-11-18 20:11:55 -0300

    Cadastro,limpar formulario e mensagem de erro funcionando corretamente

---
+++ alura__geek/js/conecxoesApi.js
@@ -0,0 +1,29 @@
+async function listadeProdutos() {
+    const conexao = await fetch('http://localhost:3000/produtos')
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+}
+
+async function cadastrarProdutos(imagem,nome,valor) {
+    const conexao = await fetch('http://localhost:3000/produtos', {
+        method: "POST", //Como você está enviando uma solicitação POST, é preciso declarar que está usando o método POST.
+        headers: {"Content-type": "application/json"}, // Como você está enviando dados em JSON, precisa definir um cabeçalho com o Content-Type definido como application/json.
+        body: JSON.stringify({ //Por fim, você precisa do corpo (body), que será uma única string de dados em JSON.
+            imagem: imagem,
+            nome: nome,
+            valor: valor
+        })
+    })
+    if (!conexao.ok) {
+        throw new Error("Não foi possível enviar o card")
+    }
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}
+
+
+export const conecxoesApi = {
+    listadeProdutos,
+    cadastrarProdutos
+}
---
+++ alura__geek/js/enviarDadosCards.js
@@ -0,0 +1,47 @@
+import { conecxoesApi } from "./conecxoesApi.js"
+
+const formulario = document.querySelector("[dados-formulario]")
+const fraseErro = document.querySelector('.mensagem-erro')
+const btnLimpar = document.querySelector("[botao-limpar]")
+
+async function inserirDados(evento) {
+    evento.preventDefault()
+
+    const imagem = document.querySelector("[dados-imagem]")
+    const nome = document.querySelector("[dados-nome]")
+    const valor = document.querySelector("[dados-valor]")
+
+    
+    try {
+
+        imagem.addEventListener('click',() => {
+            fraseErro.innerHTML = ""
+        })
+        nome.addEventListener('click', () => {
+            fraseErro.innerHTML = ""
+        })
+
+        valor.addEventListener('click', () => {
+            fraseErro.innerHTML = ""
+        })
+        if (imagem.value == "" || nome.value == "" || valor.value == "") {
+            
+            fraseErro.innerHTML = 'Insira um valor valido para cadastro !!!'.toLocaleUpperCase()
+        } else {
+            await conecxoesApi.cadastrarProdutos(imagem.value, nome.value, valor.value);
+            // window.location.href = "../pages/envio-concluido.html";
+        }
+    } catch (e) {
+        alert(e);
+    }
+}
+
+formulario.addEventListener("submit", evento => inserirDados(evento))
+
+btnLimpar.addEventListener('click', (e) => {
+    document.querySelector("[dados-nome]").value = ""
+    document.querySelector("[dados-valor]").value = ""
+    document.querySelector("[dados-imagem]").value = ""
+    e.preventDefault()
+})
+
