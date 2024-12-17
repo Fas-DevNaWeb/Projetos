2134d9bf3348db1dbb8e9cbca10ccaacc3847b11
Fabiano <fasnawebdev@gmail.com>
2024-12-16 23:06:47 -0300

    continuando projeto

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,15 +1,71 @@
-import Banner from './Componentes/Banner';
-import { Formulario } from './Componentes/Formulario/Formulario';
+import { useState } from "react";
+import Banner from "./Componentes/Banner";
+import { Formulario } from "./Componentes/Formulario/Formulario";
+import Time from "./Componentes/Time";
+
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria:'#57C278',
+      corSecundaria:'#D9F7E9'
+    },
+    {
+      nome: 'Front End',
+      corPrimaria:'#82CFFA',
+      corSecundaria:'#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria:'#A6D157',
+      corSecundaria:'#F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria:'#E06B69',
+      corSecundaria:'#FDE7EB'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria:'#D86EBF',
+      corSecundaria:'#FAE9FS'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria:'#FFBA05',
+      corSecundaria:'#FFF5D9'
+    },
+    {
+      nome: 'Inavação e Gestão',
+      corPrimaria:'#FFFA29',
+      corSecundaria:'#FFEEDF'
+    }
+  ]
+
+  const [colaboradores, setColaboradores] = useState("");
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores,colaborador])
+    console.log(colaborador);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <Banner />
-        <Formulario />
+        <Formulario
+          times={times.map(time => time.nome)}
+          aoColaboradorCadastrado = {colaborador =>
+            aoNovoColaboradorAdicionado(colaborador)
+          }
+        />
+       
+       {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
       </header>
     </div>
   );
 }
- 
+
 export default App;
---
+++ organo/src/Componentes/Colaborador/Colaborador.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+export const Colaborador = (props) => {
+  return (
+    <div>
+      <div>
+        <img src="http://github.com/viniciosneves.png" alt="Foto do Vinicios" />
+      </div>
+      <div>
+        <h4>Vinicios Neves</h4>
+        <h5>Instrutor</h5>
+      </div>
+    </div>
+  );
+};
+
+
