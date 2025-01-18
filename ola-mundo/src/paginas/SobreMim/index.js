75a28e36680abc0e5a208d876c776221895c2b16
Fabiano <fasnaweb2020@gmail.com>
2025-01-18 13:45:33 -0300

    vamos denovo]

--- ola-mundo/src/paginas/SobreMim/index.js
+++
@@ -1,7 +0,0 @@
-import styles from "./SobreMim.module.css"
-
-export default function SobreMim(){
-    return(
-        <h1>Componente Sobre Mim</h1>
-    )
-}
--- ola-mundo/src/paginas/inicio/index.js
+++
@@ -1,7 +0,0 @@
-import styles from "./Inicio.module.css"
-
-export default function Inicio(){
-    return(
-        <h1>Componente Inicio !!!</h1>
-    )
-}
